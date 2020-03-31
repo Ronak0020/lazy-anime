cbd79664eaabca26e5d47a458c09767ee54e766c
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-03-31 13:52:49 +0530

    Update cstats.js

--- commands/info/cstats.js
+++ commands/info/cstats.js
@@ -5,18 +5,18 @@ module.exports = {
     name: "cstats",
     category: "info",
     description: "View the stats for corona cases.",
-    usage: "<all | country <name> | state <name> >",
+    usage: "<world | country <name> | state <name> >",
     aliases: ["corona", "covid-19", "coronastats", "cstat"],
     run: async(client, message, args) => {
         const all = await covid.getAll();
-        const country = await covid.getCountry({country: args.slice(1).join(" ")}).catch(message.channel.send("Data was not found!"));
-        const state = await covid.getState({state: args.slice(1).join(" ")}).catch(e => message.channel.send("Data was not found!"));
+        const country = await covid.getCountry({country: args.slice(1).join(" ")});
+        const state = await covid.getState({state: args.slice(1).join(" ")});
         const embed = new Discord.RichEmbed()
         .setTitle("🌎 COVID-19 UPDATES")
         .setFooter(client.user.username, client.user.displayAvatarURL)
         .setDescription(`**Total World cases :** ${all.cases}\n**Total Deaths :** ${all.deaths}\n**Total Recovered :** ${all.recovered}`)
         .setColor("RANDOM")
-        if(!args[0]) return message.channel.send(embed);
+        if(args[0] === "world") return message.channel.send(embed);
 
         const countrye = new Discord.RichEmbed()
         .setTitle("🌎 COVID-19 UPDATES - " + args.slice(1).join(" "))
