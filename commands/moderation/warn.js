cbd6d99943c1a97f138bbb60d468ee8be4687662
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-04-12 11:55:19 +0530

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const mongoose = require('mongoose');
 const dbURL = process.env.MONGODBURL;
+const { formatDate } = require("../../functions.js");
 mongoose.connect(dbURL, {
     useNewUrlParser: true
 })
@@ -12,11 +13,12 @@ module.exports = {
     category: "moderation",
     description: "Warn a member for breaking rules!",
     run: async(client, message, args) => {
-        const target = message.mentions.users.first() || message.guild.members.get(args[0]);
+        const created = formatDate(message.createdAt);
+        const target = message.mentions.members.first() || message.guild.members.get(args[0]);
+        if(target.hasPermission(["ADMINISTRATOR"])) return message.reply("U can not warn this person!");
         if(!target) return message.reply("Whom you wanna warn? Please mention the user.").then(m => m.delete(5000));
         const reason = args.slice(1).join(" ");
         if(!reason) return message.reply("Please provide a reason to warn!").then(m => m.delete(5000));
-        if(target.hasPermission(["ADMINISTRATOR"])) return message.reply("You can't warn an Admin!").then(m => m.delete(5000));
         if(!message.member.hasPermission(["MANAGE_MESSAGES"])) return message.reply("You do not have permission to warn a member.\nPermission required : `MANAGE_MESSAGES`");
         Warn.findOne({
             guildID: message.guild.id,
@@ -28,17 +30,17 @@ module.exports = {
                     guildID: message.guild.id,
                     userID: target.id,
                     mod: message.author.username,
-                    reason: reason + ` (${message.createdAt})`
+                    reason: reason + ` (${created})`
                 })
                 await newWarn.save().catch(e => console.log(e));
             } else if(user) {
-                user.reason = user.reason + "\n" + reason + `  (${message.createdAt})`,
+                user.reason = user.reason + "\n" + reason + `  (${created})`,
                 user.mod = user.mod + "\n" + message.author.username
                 await user.save().catch(e => console.log(e));
             }
             const embed = new Discord.RichEmbed()
             .setTitle("⚠ You have been warned! ⚠")
-            .setDescription(`**Server -** ${message.guild.name}\n**Moderator -** ${message.author.username}\n**Reason -** ${reason}\n**Date -** ${message.createdAt}`)
+            .setDescription(`**Server -** ${message.guild.name}\n**Moderator -** ${message.author.username}\n**Reason -** ${reason}\n**Date -** ${created}`)
             .setColor("RED")
             .setFooter(client.user.username, client.user.displayAvatarURL)
             target.send(embed);
