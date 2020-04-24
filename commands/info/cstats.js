7dd8b39352bee340b6ce3e693a76f891ed1ecca0
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-04-24 13:37:56 +0530

    Update cstats.js

--- commands/info/cstats.js
+++ commands/info/cstats.js
@@ -1,38 +1,38 @@
 const Discord = require('discord.js');
-const covid = require('covidtracker');
+const fetch = require("snekfetch");
 
 module.exports = {
-    name: "cstats",
+    name: "corona",
     category: "info",
     description: "View the stats for corona cases.",
-    usage: "<world | country <name> | state <name> >",
-    aliases: ["corona", "covid-19", "coronastats", "cstat"],
+    usage: "<world | country <name> >",
+    aliases: ["covid-19", "coronastats", "cstats", "covid"],
     run: async(client, message, args) => {
-        const all = await covid.getAll();
-        const country = await covid.getCountry({country: args.slice(1).join(" ")});
-        const state = await covid.getState({state: args.slice(1).join(" ")});
-        const embed = new Discord.RichEmbed()
-        .setTitle("🌎 COVID-19 UPDATES")
-        .setFooter(client.user.username, client.user.displayAvatarURL)
-        .setDescription(`**Total World cases :** ${all.cases.toLocaleString()}\n**Total Deaths :** ${all.deaths.toLocaleString()}\n**Total Recovered :** ${all.recovered.toLocaleString()}`)
-        .setColor("RANDOM")
-        if(args[0] === "world") return message.channel.send(embed);
-
-        const countrye = new Discord.RichEmbed()
-        .setTitle("🌎 COVID-19 UPDATES - " + args.slice(1).join(" "))
-        .setFooter("Made by Ron")
-        .setTimestamp()
-        .setDescription(`**Total cases :** ${country.cases.toLocaleString()}\n**Today's Cases : ** ${country.todayCases.toLocaleString()}\n**Total Deaths :** ${country.deaths.toLocaleString()}\n**Today's Deaths : ** ${country.todayDeaths.toLocaleString()}\n**Total Recovered :** ${country.recovered.toLocaleString()}\n**Active Cases : ** ${country.active.toLocaleString()}\n**Critical Cases : ** ${country.critical.toLocaleString()}\n`)
-        .setColor("RANDOM")
-        if(args[0] === "country") return message.channel.send(countrye);
-
-        const statee = new Discord.RichEmbed()
-        .setTitle("🌎 COVID-19 UPDATES - " + args.slice(1).join(" "))
-        .setFooter("Made by Ron")
-        .setTimestamp()
-        .setDescription(`**Total cases :** ${state.cases.toLocaleString()}\n**Today's Cases : ** ${state.todayCases.toLocaleString()}\n**Total Deaths :** ${state.deaths.toLocaleString()}\n**Today's Deaths : ** ${state.todayDeaths.toLocaleString()}\n**Total Recovered :** ${state.recovered.toLocaleString()}\n**Active Cases : ** ${state.active.toLocaleString()}\n**Critical Cases : ** ${state.critical.toLocaleString()}\n`)
-        .setColor("RANDOM")
-        if(args[0] === "state") return message.channel.send(statee);
+        if(args[0].toLowerCase() === "world") {
+        fetch.get("https://corona.lmao.ninja/v2/all?yesterday=false").then(data => {
+            const body = data.body;
+            const world = new Discord.RichEmbed()
+            .setTitle("🌎 COVID-19 UPDATES")
+            .setColor("RED")
+            .setDescription(`**Total Cases:** ${body.cases}\n**Today's Cases:** ${body.todayCases}\n**Total Deaths:** ${body.deaths}\n**Today's Deaths:** ${body.todayDeaths}\n**Recovered:** ${body.recovered}\n**Critical cases:** ${body.critical}\n**Total Active Cases:** ${body.active}`)
+            .setFooter("Developer: Ronak#0020")
+            .setTimestamp()
+            message.channel.send(world)
+        })
+    } else if(args[0].toLowerCase() === "country") {
+        if(!args[1]) return message.reply("Please provide the name of the country with the command.").then(m => m.delete(5000));
+        fetch.get(`https://corona.lmao.ninja/v2/countries/${args[1]}?yesterday=false&strict=false`).then(data => {
+            const body = data.body;
+            const country = new Discord.RichEmbed()
+            .setTitle("🌎 COVID-19 UPDATES - " + args[1])
+            .setThumbnail(body.countryInfo.flag)
+            .setColor("RED")
+            .setDescription(`**Total Cases:** ${body.cases}\n**Today's Cases:** ${body.todayCases}\n**Total Deaths:** ${body.deaths}\n**Today's Deaths:** ${body.todayDeaths}\n**Recovered:** ${body.recovered}\n**Critical cases:** ${body.critical}\n**Total Active Cases:** ${body.active}`)
+            .setFooter("Developer: Ronak#0020")
+            .setTimestamp()
+            message.channel.send(country)
+        })
+    }
         
     }
 }
