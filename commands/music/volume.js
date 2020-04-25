88bf679dc983741fcce42c15a5eb9b828a76ee64
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-04-25 13:13:50 +0530

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -11,7 +11,6 @@ module.exports = {
   run: async(client, message, args) => {
       const vol = args[0];
       if(!vol) return message.reply("Please provide the volume you want. It must be an integer.").then(m => m.delete(5000));
-      if(isNaN(vol)) return message.reply("The provided volume is not an integer!");
       musicPlayer.volume(message, vol)
   }
 }
