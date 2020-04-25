d4a9f8af4dede2d6f7ce79a96e4c3ffa5fc19777
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-04-25 12:46:39 +0530

    Create play.js

---
+++ commands/music/play.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const MusicClient = require("la-music-core");
+const musicPlayer = new MusicClient(process.env.YTKEY);
+
+module.exports = {
+  name: "play",
+  run: async(client, message, args) => {
+      const searchArray = args.join(" ");
+    if(!searchArray) return message.reply("You need to provide me a song name or YT link to play.").then(m => m.delete(5000));
+      musicPlayer.play(message, searchArray)
+  }
+}
