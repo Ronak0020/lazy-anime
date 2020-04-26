57ee8883286500d8ca5995465eb7bd9d3149e395
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-04-26 11:52:22 +0530

    Update captchafun.js

--- commands/image fun/captchafun.js
+++ commands/image fun/captchafun.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { get } = require('snekfetch');
 
 module.exports = {
-    name: "cap",
+    name: "captcha",
     usage: "[@user]",
     description: "Get a captcha for a user\'s avatar!",
     category: "image fun",
