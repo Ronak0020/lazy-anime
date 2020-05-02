57d4ddc35cf90d9970ccf64a06bc9b5a4faa9942
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-05-02 20:38:47 +0530

    Create battle.js

---
+++ utils/battle.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+const dbUrl = process.env.MONGODBURL;
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true
+});
+const Profile = require("../modules/battleprofile.js");
+
+module.exports.recordSave = (message, winner, loser) => {
+    Profile.findOne({
+        userID: winner.id,
+        serverID: message.guild.id
+    }, (err, res) => {
+        if (err) console.log(err);
+        if (!res) {
+            const newDoc = new Profile({
+                userID: winner.id,
+                username: winner.user.username,
+                serverID: message.guild.id,
+                wins: 1,
+                losses: 0
+            })
+            newDoc.save().catch(err => console.log(err));
+        } else {
+            res.wins = res.wins + 1;
+            res.save().catch(err => console.log(err));
+        }
+        Profile.findOne({
+            userID: loser.id,
+            serverID: message.guild.id
+        }, (err, res) => {
+            if(err) console.log(err)
+            if(!res){
+                const newDoc = new Profile({
+                    userID: loser.id,
+                    username: loser.user.username,
+                    serverID: message.guild.id,
+                    wins: 0,
+                    losses: 1
+                })
+                newDoc.save().catch(err => console.log(err));
+            }else{
+                res.losses = res.losses + 1;
+                res.save().catch(err => console.log(err));
+            }
+        })
+    });
+}
