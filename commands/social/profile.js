a581c19a716fd1caca76f34ce43041b65a97fc94
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-05-03 12:04:17 +0530

    Update profile.js

--- commands/social/profile.js
+++ commands/social/profile.js
@@ -58,10 +58,14 @@ module.exports = {
                         .setFooter("Ronak's Creation")
                         .setDescription(`**User Name:** ${target.username}\n**Profile Title:** ${user.title}\n**About:** ${user.about}\n**Rep:** ${user.rep.toLocaleString()}`)
                         .addField("Economy Stats:", `**Cash:** ${money.coins.toLocaleString()}\n**Bank:** ${money.bank.toLocaleString()}`)
-                        .addField("Level Stats:", `**Server Level:** ${member.level}\n**Server XP:** ${member.xp.toLocaleString()}`)
                         .addField("Pet Stats:", `**Pet Name:** ${pet.petName}\n**Pet Trait:** ${pet.petTrait}\n**Pet Level:** ${pet.petLvl}`)
-                        .addField("Battle Stats:", `**Battles Won:** ${bpro.wins}\n**Battles Lost:** ${bpro.losses}\n**Class:** ${bpro.class}`)
-                        message.channel.send(embed)
+                        .addField("Battle Stats:", `**Battles Won:** ${bpro.wins}\n**Battles Lost:** ${bpro.losses}\n**Class:** ${bpro.class}`);
+                        if(!member) {
+                            message.channel.send(embed)
+                        }
+                        //.addField("Level Stats:", `**Server Level:** ${member.level}\n**Server XP:** ${member.xp.toLocaleString()}`)
+                        
+                        //message.channel.send(embed)
                     })
                 })
             })
