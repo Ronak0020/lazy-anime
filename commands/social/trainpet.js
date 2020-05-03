969705616a0cbc3a35dbc74604c0e28ef46eb8f8
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-05-03 11:36:13 +0530

    Update trainpet.js

--- commands/social/trainpet.js
+++ commands/social/trainpet.js
@@ -3,6 +3,7 @@ const Pet = require("../../modules/pets.js")
 const petUtil = require("../../utils/pets.js")
 const mongoose = require("mongoose");
 const dbUrl = process.env.MONGODBURL;
+const cooldown = new Set();
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -13,6 +14,9 @@ module.exports = {
     description: "Train your pet to level it up",
     category: "social",
     run: async (client, message, args) => {
+    if (cooldown.has(message.author.id)) {
+            message.channel.send("Wait `1 minute` before using this command again. - " + message.author);
+    } else {
     //this is where the actual code for the command goes
     Pet.findOne({
         ownerID: message.author.id
@@ -30,5 +34,11 @@ module.exports = {
             return message.reply("You hit " + data.damage + " and gained " + data.experience + " experience.")
         }
     });
+        if(message.author.id === "625877119989186570") return;
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+          cooldown.delete(message.author.id);
+        }, 60000);
+    }
 }
 }
