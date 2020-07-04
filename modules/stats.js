591a5adc8d36f7ffc4aa6d04640785cc720b121d
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2020-07-04 20:43:16 +0530

    Update stats.js

--- modules/stats.js
+++ modules/stats.js
@@ -1,11 +1,11 @@
 const {Schema, model} = require("mongoose");
 
 const STATS = new Schema({
-roleID: String,
-messages: Number,
-eventwins: Number,
-top: String,
-blacklisted: []
+    roleID: String,
+    messages: Number,
+    eventwins: Number,
+    top: String,
+    blacklisted: Array
 });
 
-module.exports = model("LAclubsStats", STATS);
+module.exports = model("clubstatistics", STATS);
